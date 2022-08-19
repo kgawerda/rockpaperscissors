1d66ae5b12b7cc9a97dba0caa75f16e22438c994
Konrad <konradgawerda@gmail.com>
2022-08-19 16:45:21 +0200

    Add best out of 5 game

--- javascript.js
+++ javascript.js
@@ -21,3 +21,54 @@ function playRound(playerSelect, computerSelect){ //-1 - computer wins, 1 - huma
     return 99;
 }
 
+let wins=0;
+let losses=0;
+for(let i=0; i<5; i++){
+    if(wins>=3){
+        console.log('You win! Congratulations!');
+        break;
+    }
+    else if(losses>=3){
+        console.log('Take the L, loser');
+        break;
+    }
+    console.log("가위바위보!");
+    let computerSelect=getComputerChoice();
+    let playerChoice=prompt("Choose your fighter!");
+    let playerSelect;
+    switch(playerChoice.toLowerCase()){
+        case 'rock':
+            playerSelect=0;
+            break;
+        case 'paper':
+            playerSelect=1;
+            break;
+        case 'scissors':
+            playerSelect=2;
+            break;
+        default:
+            console.log('Smth wrong');
+    }
+    let score=playRound(playerSelect, computerSelect);
+    switch(score){
+        case 0:
+            console.log("Remis!")
+            console.log('Current score:');
+            console.log('Player '+wins+'-'+losses);
+            break;
+        case 1:
+            wins++;
+            console.log("You win this time")
+            console.log('Current score:');
+            console.log('Player '+wins+'-'+losses);
+            break;
+        case -1:
+            losses++;
+            console.log("You lose this time")
+            console.log('Current score:');
+            console.log('Player '+wins+'-'+losses);
+            break;
+        default:
+            console.log('Smth wrong');
+    }
+}
